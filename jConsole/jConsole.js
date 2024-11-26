4c8e6ded1edcb47cbee10d3b15b080548f9a231b
Andrew Feasel <feaseand000@student.evergreenps.org>
2024-11-26 13:13:49 -0800

    Add files via upload

---
+++ jConsole/jConsole.js
@@ -0,0 +1,92 @@
+const $ = (x) => document.getElementById(x);
+function start(){
+    const myCodeMirror = CodeMirror.fromTextArea($('code'), {
+        mode: 'javascript',
+        indentUnit: 4,
+        lineNumbers: true,
+        spellcheck: false,
+        autocorrect: false,
+        autocapitalize: false,
+        indentWithTabs: true,
+        allowDropFileTypes: ['text/javascript','text/css','text/html'],
+    });
+    try {
+        myCodeMirror.setOption("extraKeys", {
+            Tab: function(cm) {
+                const spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
+                cm.replaceSelection(spaces);
+            }
+        });
+        myCodeMirror.on("change", (e) =>
+            $('input').value = myCodeMirror.getValue()
+        );
+        const fileInput = $('file');
+        fileInput.addEventListener("change", async () => {
+            const [file] = fileInput.files;
+            if (file) {
+                try {
+                    const textContent = await file.text();
+                    myCodeMirror.setValue(textContent);
+                } catch (error) {
+                    logError(error);
+                }
+            }
+        });
+    } catch (e) {
+        jConsole.logError(e);
+    }
+}
+const jConsole = {
+    log(x) {
+        try {
+            const console = $("consoleUI");
+            console.innerHTML += JSON.stringify(x, null, 2) + '<br>';
+            console.scrollTop = console.scrollHeight;
+        } catch (e) {
+            this.logError(e);
+        }
+    },
+    clear() {
+        const console = $("consoleUI");
+        console.innerHTML = '';
+        console.scrollTop = consoleElement.scrollHeight;
+    },
+    logError(error) {
+        this.log(`${error.name}; ${error.message}.`);
+    },
+    eval() {
+        try {
+            const input = $('input');
+            if (input.value === '') {
+                throw new ReferenceError('No code');
+            }
+            eval(input.value);
+        } catch (e) {
+            this.logError(e);
+        }
+    }
+};
+function getProtocol(url) {
+    let proto = url.match(/^([A-Za-z]+):/);
+    if (proto) {
+        return proto[1].toLowerCase();
+    } else {
+        return 'None';
+    }
+}
+async function request(url){
+    jConsole.log('Awaiting Response...');
+    try {
+        switch(getProtocol(url)){
+            case 'None':
+                url = 'https://' + url;
+                break;
+        }
+        const response = await fetch(url);
+        jConsole.log(`Response Code: ${response.status}`)
+        const json = await response.json();
+        jConsole.log(json);
+    } catch (e) {
+        jConsole.logError(e);
+    }
+}
