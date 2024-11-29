0944f6f0aab1f115ed2a590a95c965805c365a52
Andrew Feasel <feaseand000@student.evergreenps.org>
2024-11-28 18:37:30 -0800

    f

--- jConsole/jConsole.js
+++ jConsole/jConsole.js
@@ -1,23 +1,28 @@
 const $ = (x) => document.getElementById(x);
 document.addEventListener("DOMContentLoaded", (event) => {
     try{
+        alert(1);
         const input = $('input').value;
         const logButton = $("logButton");
         logButton.onclick = () => {
             jConsole.log(input);
         };
+        alert(2);
         const evalButton = $("evalButton");
         evalButton.onclick = () => {
             eval(input);
         };
+        alert(3);
         const clearButton = $("clearButton");
         clearButton.onclick = () => {
             jConsole.clear();
         };
+        alert(4);
         const requestButton = $("requestButton");
         requestButton.onclick = () => {
             httpRequest($("url").value);
         };
+        alert(5);
         const myCodeMirror = CodeMirror.fromTextArea($('code'), {
             mode: 'javascript',
             indentUnit: 4,
