c91d7c2e89a0b9ed8a08a64ffbc2f9b9f79aa72b
Andrew Feasel <feaseand000@student.evergreenps.org>
2024-12-19 11:50:07 -0800

    Update script.js

--- jConsole/script.js
+++ jConsole/script.js
@@ -1,1 +1,54 @@
-
+document.addEventListener("DOMContentLoaded", (event) => {
+    try {
+        const logButton = $("logButton");
+        const evalButton = $("evalButton");
+        const clearButton = $("clearButton");
+        const requestButton = $("requestButton");
+        const fileInput = $('file');
+        const code = $('code');
+        const myCodeMirror = CodeMirror.fromTextArea(code, {
+            mode: 'javascript',
+            indentUnit: 4,
+            lineNumbers: true,
+            spellcheck: false,
+            autocorrect: false,
+            autocapitalize: false,
+            indentWithTabs: true,
+            allowDropFileTypes: ['text/javascript','text/css','text/html']
+        });
+        myCodeMirror.setOption("extraKeys", {
+            Tab: function(cm) {
+                const spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
+                cm.replaceSelection(spaces);
+            }
+        });
+        myCodeMirror.on("change", (e) => {
+            $('input').value = myCodeMirror.getValue();
+        });
+        logButton.onclick = () => {
+            jConsole.log($('input').value);
+        };
+        evalButton.onclick = () => {
+            eval($('input').value);
+        };
+        clearButton.onclick = () => {
+            jConsole.clear();
+        };
+        requestButton.onclick = () => {
+            request($("url").value);
+        };
+        fileInput.addEventListener("change", async () => {
+            const [file] = fileInput.files;
+            if (file) {
+                try {
+                    const textContent = await file.text();
+                    myCodeMirror.setValue(textContent);
+                } catch (error) {
+                    jConsole.logError(error);
+                }
+            }
+        });
+    } catch (e) {
+        jConsole.logError(e);
+    }
+});
