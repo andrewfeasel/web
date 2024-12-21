caf74902afcb9c3da439ac493eb20e5e2d21bbbc
Andrew Feasel <feaseand000@student.evergreenps.org>
2024-12-21 11:43:22 -0800

    Update init.js

--- jConsole/init.js
+++ jConsole/init.js
@@ -3,7 +3,7 @@ const jConsole = {
     log(x) {
         try {
             const ui = $("consoleUI");
-            ui.innerHTML += JSON.stringify(x, null, 2) + '<br>';
+            ui.innerHTML += x + '<br>';
             ui.scrollTop = ui.scrollHeight;
         } catch (error) {
             this.logError(error);
@@ -18,5 +18,17 @@ const jConsole = {
         this.log(`${error.name} ${error.message} ${error.stack}`);
     }
 };
+window.addEventListener("offline", (event) => {
+  jConsole.log("offline");
+});
+window.addEventListener("online", (event) => {
+  jConsole.log("online");
+});
 const observer = lozad();
 observer.observe();
+window.addEventListener("offline", (event) => {
+  jConsole.log("offline");
+});
+window.addEventListener("online", (event) => {
+  jConsole.log("online");
+});
