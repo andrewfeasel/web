6373feb66ff458dc99bf131fde18fc7bfffecd1e
Andrew Feasel <feaseand000@student.evergreenps.org>
2024-12-21 21:00:13 -0800

    Update init.js

--- jConsole/init.js
+++ jConsole/init.js
@@ -11,20 +11,3 @@ const jConsole = {
         ui.scrollTop = ui.scrollHeight;
     }
 };
-async function request(url){
-    const response = await fetch(url, {
-        headers: {"Content-Type":"application/json"}
-    });
-    const json = await response.json();
-    return json;
-}
-async function post(url, data){
-	const content = JSON.stringify(data,null,2);
-    const response = await fetch(url,{
-	    method: "POST",
-		headers: {"Content-Type":"application/json"},
-		body: content
-	});
-    const json = await response.json();
-    return json;
-}
